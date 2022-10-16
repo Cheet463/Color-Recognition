fc2b4cea7adbd47ffec43c16afc0be67c91e0e52
Alberto <albertovalleflores99@gmail.com>
2022-10-16 16:25:34 -0700

    Fix data generator

--- data_gen.py
+++ data_gen.py
@@ -5,23 +5,23 @@ import cv2
 import numpy as np
 import os.path
 
-# lower boundary RED color range values; Hue (0 - 10)
-LOWER1 = np.array([0, 100, 20])
-UPPER1 = np.array([10, 255, 255])
-
-# upper boundary RED color range values; Hue (160 - 180)
-LOWER2 = np.array([160, 100, 20])
-UPPER2 = np.array([179, 255, 255])
-
-pixel_count = 0
-
-HEADERS = ["Date", "Count"]
-
-FILENAME = 'data.csv'
-file_exists = os.path.isfile(FILENAME)
-
 
 def start():
+    # lower boundary RED color range values; Hue (0 - 10)
+    LOWER1 = np.array([0, 100, 20])
+    UPPER1 = np.array([10, 255, 255])
+
+    # upper boundary RED color range values; Hue (160 - 180)
+    LOWER2 = np.array([160, 100, 20])
+    UPPER2 = np.array([179, 255, 255])
+
+    white_pix_count = 0
+
+    HEADERS = ["Date", "Count"]
+
+    FILENAME = 'data.csv'
+    file_exists = os.path.isfile(FILENAME)
+
     with open(FILENAME, 'w') as new_csv_file:
         csv_writer = csv.DictWriter(new_csv_file,
                                     delimiter=',',
@@ -33,10 +33,12 @@ def start():
 
     # Captures video from file
     cap = cv2.VideoCapture('IMG_1968.MOV')
+    frame_count = 0
 
-    while True:
+    while cap.isOpened():
+
         # Take each frame
-        _, frame = cap.read()
+        is_success, frame = cap.read()
 
         # Convert BGR to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -56,23 +58,27 @@ def start():
         cv2.imshow('result', result)
 
         # counting the number of pixels
-        number_of_white_pix = np.sum(result == 255)
+        white_pix_count += np.sum(result == 255)
         # number_of_black_pix = np.sum(result == 0)
+        frame_count += 1
+        print(datetime.now(), white_pix_count)
+        if frame_count == 10:
+            with open(FILENAME, 'a') as csv_file:
+                update_writer = csv.DictWriter(csv_file,
+                                               delimiter=',',
+                                               lineterminator='\n',
+                                               fieldnames=HEADERS)
 
-        with open(FILENAME, 'a') as csv_file:
-            update_writer = csv.DictWriter(csv_file,
-                                           delimiter=',',
-                                           lineterminator='\n',
-                                           fieldnames=HEADERS)
+                info = {
+                    "Date": datetime.now(),
+                    "Count": white_pix_count/10
+                }
 
-            info = {
-                "Date": datetime.now(),
-                "Count": number_of_white_pix
-            }
-
-            update_writer.writerow(info)
-            print(datetime.now(), number_of_white_pix)
-            time.sleep(1)
+                update_writer.writerow(info)
+                print(f"Average is {white_pix_count/10}")
+                time.sleep(1)
+                frame_count = 0
+                white_pix_count = 0
 
 
 if __name__ == '__main__':
